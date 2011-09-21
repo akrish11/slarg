7f7e18a4cec7eaaaf04fe268bc87c44a423b668a
Akshay <akska8@gmail.com>
2011-09-21 00:21:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slarg::Application.config.secret_token = '5450763b935dd2648375d8e20e40372253e9de73135e3c8e941a650d91df5f2b8c6e5f077ba45717d08f875d449411a3351aa300c42e955ff5f479739425d742'
